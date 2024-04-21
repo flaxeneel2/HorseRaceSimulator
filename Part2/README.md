2445be8b3f9ae2758e9fcfbb8090d284e4b7cee3
flaxeneel2 <flaxeneel2@gmail.com>
2024-04-21 03:25:09 +0100

    Integrate part 2 into main maven project

--- Part2/README.md
+++ Part2/README.md
@@ -1,3 +1,3 @@
 # Part 2
 
-You are on the main branch! Check out part 2 at the [gui-development branch](https://github.com/flaxeneel2/HorseRaceSimulator/tree/gui-development/Part2)
+This is the UI code
---
+++ Part2/src/main/java/net/flaxeneel2/uni/sem2/oop/coursework/Testarea.java
@@ -0,0 +1,89 @@
+package net.flaxeneel2.uni.sem2.oop.coursework;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Testarea extends JFrame {
+    private int pixelSize = 10;
+    private Color currentColor = Color.BLACK;
+    private JPanel canvas;
+
+    public Testarea() {
+        setTitle("Draw your horse");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        canvas = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                int width = getWidth();
+                int height = getHeight();
+                for (int x = 0; x < width; x += pixelSize) {
+                    for (int y = 0; y < height; y += pixelSize) {
+                        g.setColor(Color.WHITE);
+                        g.fillRect(x, y, pixelSize, pixelSize);
+                        g.setColor(Color.BLACK);
+                        g.drawRect(x, y, pixelSize, pixelSize);
+                    }
+                }
+            }
+        };
+        canvas.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                drawPixel(e);
+            }
+        });
+        canvas.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                drawPixel(e);
+            }
+        });
+
+        JPanel controlPanel = new JPanel();
+        JButton colorButton = new JButton("Choose Color");
+        colorButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentColor = JColorChooser.showDialog(null, "Choose Color", currentColor);
+            }
+        });
+        JButton clearButton = new JButton("Clear Canvas");
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearCanvas();
+            }
+        });
+        controlPanel.add(colorButton);
+        controlPanel.add(clearButton);
+        controlPanel.add(new JButton("Save"));
+
+        setLayout(new BorderLayout());
+        add(canvas, BorderLayout.CENTER);
+        add(controlPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    private void drawPixel(MouseEvent e) {
+        int x = e.getX() / pixelSize * pixelSize;
+        int y = e.getY() / pixelSize * pixelSize;
+        Graphics g = canvas.getGraphics();
+        g.setColor(currentColor);
+        g.fillRect(x, y, pixelSize, pixelSize);
+    }
+
+    private void clearCanvas() {
+        Graphics g = canvas.getGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Testarea::new);
+    }
+}
