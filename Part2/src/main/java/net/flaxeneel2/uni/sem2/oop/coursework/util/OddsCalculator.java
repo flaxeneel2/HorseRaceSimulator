5fa8dcb9a2ae5e2f75850dc787ae92623fb3021d
flaxeneel2 <flaxeneel2@gmail.com>
2024-04-25 02:11:26 +0100

    Implemented odds calculation

--- Part2/src/main/java/net/flaxeneel2/uni/sem2/oop/coursework/util/OddsCalculator.java
+++ Part2/src/main/java/net/flaxeneel2/uni/sem2/oop/coursework/util/OddsCalculator.java
@@ -10,31 +10,33 @@ public class OddsCalculator {
         double totalAvgSpeed = 0;
         double totalWinRate = 0;
 
+        double totalOdds = 0;
+
         for (Horse horse : horses) {
             totalDistance += horse.getHorseData().getTotalDistanceTravelled();
             totalAvgSpeed += horse.getHorseData().getAverageSpeed();
             totalWinRate += getWinRate(horse);
         }
-
-        double overallWeight = totalDistance + totalAvgSpeed + totalWinRate;
-
         for (Horse horse : horses) {
             double distanceWeight = (double) horse.getHorseData().getTotalDistanceTravelled() / totalDistance;
             double speedWeight = horse.getHorseData().getAverageSpeed() / totalAvgSpeed;
             double winRateWeight = getWinRate(horse) / totalWinRate;
 
-            double horseWeight = distanceWeight + speedWeight + winRateWeight;
+            double overallWeight = distanceWeight + speedWeight + winRateWeight;
 
-            // Calculate odds
-            double horseOdds = horseWeight / overallWeight;
+            double horseOdds = overallWeight / (totalDistance + totalAvgSpeed +  totalWinRate);
             if(Double.isNaN(horseOdds)) {
-                horse.setOdds(1.0/horses.size());
+                horse.setOdds(1.0);
+                totalOdds = horses.size();
             } else {
+                totalOdds+=horseOdds;
                 horse.setOdds(horseOdds);
             }
         }
 
-
+        for(Horse horse : horses) {
+            horse.setOdds(horse.getOdds()/totalOdds);
+        }
 
     }
 
